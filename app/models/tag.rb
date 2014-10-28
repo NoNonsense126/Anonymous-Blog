220041d9493766b875dbf12ae31249adfac14559
NoNonsense126 <retail_nns@hotmail.com>
2014-10-28 16:55:46 +0800

    Downcase tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,11 @@ class Tag < ActiveRecord::Base
 	has_many :posts, :through => :posts_to_tag
 	has_many :posts_to_tag
   validates :name, :presence => true, :uniqueness => true
+  before_create :downcase_tag
 
+  private 
+  def downcase_tag
+  	self.name = self.name.downcase
+  end
 
 end
